06d44a0ded1510eadf3632cea53139f02e1a4b0e
준혁 신 <sjh8236@naver.com>
2021-03-08 21:58:31 +0900

    초기 커밋.

---
+++ ServerCore/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void MainThread(object state)
+        {
+            for (int i =0; i<5; i++)
+                Console.WriteLine("Hello Thread!");
+        }
+        static void Main(string[] args)
+        {
+
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning );
+                t.Start();
+            }
+
+            //for (int i = 0; i < 5; i++)
+              //  ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+            
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //for (int i = 0; i < 1000; i++)
+            //{
+
+            //    Thread t = new Thread(MainThread);
+            //    t.Name = "Test Thread";
+            //    //t.IsBackground = true;
+            //    t.Start();
+            //}
+            
+            //Console.WriteLine("Waiting for Thread!");
+
+            //t.Join();
+            //Console.WriteLine("Hello World!");
+            while (true)
+            {
+
+            }
+        }
+    }
+}
