d6947c664173b70eef59d1c5d936a37870c25fdf
준혁 신 <sjh8236@naver.com>
2021-03-10 20:18:17 +0900

    DeadLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,31 +4,69 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    // 메모리 베리어 예제
+    class FastLock
+    {
+        public int id;
+    }
 
+    class SessionManager
+    {
+        static object _lock = new object();
+        public static void TestSession()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+        public static void TestUser()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+    }
     class Program
     {
         static volatile int number = 0;
+        static object _obj = new object();
 
         static void Thread_1()
         {
-
-            // atomic = 원자성 --> 지켜지지 않으면 아이템 복사가 일어난다.
-
-            // 집행검 User 2 인벤에 넣어라 - OK
-            // 집행검 User 1 인벤에서 없애라 - fail
-
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                // All or Nothing
-                Interlocked.Increment(ref number);
+                SessionManager.Test();
+
+
             }
         }
+
+        // 데드락 DeadLock
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                Interlocked.Decrement(ref number);
+                UserManager.Test();
             }
         }
 
@@ -37,6 +75,9 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(100); // 타이밍이 어긋나게해서 크래쉬를 막는다.
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
