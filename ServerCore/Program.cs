ebeb46d2ee48be6c22ee5558ea59f3a33fd7c856
준혁 신 <sjh8236@naver.com>
2021-03-10 21:59:06 +0900

    AutoResetEvent, Mutex

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,54 +4,27 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class SpinLock // 면접에서 1순위
-    {
-        volatile int _locked = 0;
-        public void Acquire()
-        {
-            while (true)
-            {
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0)
-                //    break;
-
-                //CAS: Compare-And-Swap
-                int expected = 0;
-                int desired = 1;
-                if(Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                    break;
-
-                // 쉬다 올게~
-                //Thread.Sleep(1); // 양수(1)를 넣는경우, 무조건 휴식 1ms 정도 쉬고 싶어요
-                //Thread.Sleep(0); // 0 을 넣는 경우, 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테
-                Thread.Yield(); // 관대한 양보 => 관대하게 양보할테니, 지금 실행이 가능한 스레드가 있으면 실행하세요 => 실행 가능한 애가 없으면 시간 소진
-            }
-        }
-        public void Release()
-        {
-            _locked = 0;
-        }
-    }
+    
     class Program
     {
         static int _num = 0;
-        static SpinLock _lock = new SpinLock();
+        static Mutex _lock = new Mutex();
         static void Thread_1()
         {
             for(int i =0; i< 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
         static void Thread_2()
         {
             for (int i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
         static void Main(string[] args)
