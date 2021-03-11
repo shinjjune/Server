8338bef7c06c43b782ae91eb08c2c644a4636ae3
준혁 신 <sjh8236@naver.com>
2021-03-11 23:25:57 +0900

    Thread Local Storage

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -7,34 +7,28 @@ namespace ServerCore
     class Program
     {
 
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(()=> { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
+
+        static void WhoAmI()
+        {
+            //ThreadName.Value = $"My Name Is {Thread.CurrentThread.ManagedThreadId}";
+            bool repeat = ThreadName.IsValueCreated;
+            if(repeat)
+                Console.WriteLine(ThreadName.Value + "(repeat)");
+            else
+                Console.WriteLine(ThreadName.Value);
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine(ThreadName.Value);
+        }
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                for (int i=0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count++;
-                    _lock.WriteUnlock();
-                }
-            });
-            Task t2 = new Task(delegate ()
-            {
-                for (int i=0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count--;
-                    _lock.WriteUnlock();
-                }
-            });
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
 
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-            Console.WriteLine(count);
+            ThreadName.Dispose();
         }
     }
 }
